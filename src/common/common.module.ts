acfbb1ebbcfc93b53c8539169cc28db2289677ff
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-06-03 11:24:29 -0300

    Encapsulating options in dedicated file

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,18 +1,12 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { VALIDATION_PIPE_OPTIONS } from './util/common.constants';
 
 @Module({
   providers: [
     {
       provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-        transformOptions: {
-          enableImplicitConversion: true,
-        },
-      }),
+      useValue: new ValidationPipe(VALIDATION_PIPE_OPTIONS),
     },
   ],
 })
---
+++ src/common/util/common.constants.ts
@@ -0,0 +1,10 @@
+import { ValidationPipeOptions } from '@nestjs/common';
+
+export const VALIDATION_PIPE_OPTIONS: ValidationPipeOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+  transform: true,
+  transformOptions: {
+    enableImplicitConversion: true,
+  },
+};
