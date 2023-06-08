e63d4a3e52c0ab1bb5fb9e943d3d03819e43c3a5
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-06-08 16:05:55 -0300

    Using configuration namespace and partial registration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports: [ConfigModule.forFeature(databaseConfig)],
+      inject: [databaseConfig.KEY],
+      useFactory: (
+        databaseConfiguration: ConfigType<typeof databaseConfig>,
+      ) => ({
         type: 'postgres',
-        url: process.env.DATASOURCE_URL,
+        url: databaseConfiguration.url,
         autoLoadEntities: true,
       }),
     }),
