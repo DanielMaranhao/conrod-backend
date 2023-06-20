4762e72cb6acd8b3861e5d4ad1be8a302a8b29df
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-06-20 16:24:33 -0300

    Implementing payment logic

--- src/domain/payments/payments.module.ts
+++ src/domain/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'orders/entities/order.entity';
 import { Payment } from './entities/payment.entity';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from 'orders/entities/order.entity';
+import { OrderStatus } from 'orders/enums/order-status.enum';
+import { Repository } from 'typeorm';
+import { Payment } from './entities/payment.entity';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentsRepository: Repository<Payment>,
+    @InjectRepository(Order)
+    private readonly ordersRepository: Repository<Order>,
+  ) {}
+
+  async payOrder(id: number) {
+    const order = await this.ordersRepository.findOne({
+      where: { id },
+      relations: {
+        payment: true,
+      },
+    });
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    if (order.payment) {
+      throw new ConflictException('Order already paid');
+    }
+
+    const payment = this.paymentsRepository.create();
+    order.payment = payment;
+    order.status = OrderStatus.AWAITING_SHIPMENT;
+    return this.ordersRepository.save(order);
+  }
+}
