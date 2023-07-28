bdbee69891416e1ce4b91674b8e0f0bd6d51e2e3
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-07-28 13:49:44 -0300

    Refactor in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './domain/users/users.module';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
-import { EnvModule } from './env/env.module';
+import { CategoriesModule } from './domain/categories/categories.module';
 import { OrdersModule } from './domain/orders/orders.module';
 import { PaymentsModule } from './domain/payments/payments.module';
-import { CategoriesModule } from './domain/categories/categories.module';
 import { ProductsModule } from './domain/products/products.module';
+import { UsersModule } from './domain/users/users.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrdersModule, PaymentsModule, CategoriesModule, ProductsModule],
+  imports: [
+    DatabaseModule,
+    EnvModule,
+    CommonModule,
+    UsersModule,
+    OrdersModule,
+    PaymentsModule,
+    CategoriesModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
