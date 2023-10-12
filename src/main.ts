06e59873897e18daccc170d105aed981e8cdaee1
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-10-12 09:37:34 -0300

    Adding helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
