2bc251ef23569a3f7a22ae33eba1c6267a0c0203
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-12-04 09:00:21 -0300

    Appropriate status in login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from './decorators/user.decorator';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
@@ -8,6 +14,7 @@ import { RequestUser } from './interfaces/request-user.interface';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@User() user: RequestUser) {
