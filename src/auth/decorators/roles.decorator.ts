250cbc0e394dea09af02147a7fdfe8c8d60095f3
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-12-05 16:46:16 -0300

    Empty arrays can now be forbidden

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,6 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from 'auth/roles/enums/role.enum';
+import { NonEmptyArray } from 'common/util/array.util';
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: NonEmptyArray<Role>) =>
+  SetMetadata(ROLES_KEY, roles);
