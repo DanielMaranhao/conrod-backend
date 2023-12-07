59198afa72e24eb8c7f5363bf369994e93cdd235
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2023-12-07 14:16:42 -0300

    Use throttler ttl in seconds

--- src/auth/config/throttler.config.ts
+++ src/auth/config/throttler.config.ts
@@ -4,7 +4,7 @@ import { ThrottlerModuleOptions } from '@nestjs/throttler';
 export default registerAs('throttler', () => {
   const config = [
     {
-      ttl: +process.env.THROTTLER_TTL,
+      ttl: +process.env.THROTTLER_TTL * 1000,
       limit: +process.env.THROTTLER_LIMIT,
     } as const,
   ] satisfies ThrottlerModuleOptions;
