ae9d134102f311f2d9fd215ad521ab3536694816
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-01-04 12:30:10 -0300

    Creating a swagger base document

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
 
   app.enableCors();
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('The Conrod Shop')
+    .setDescription('Documentation for the shop API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
