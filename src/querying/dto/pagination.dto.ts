0cd9b6c4386bfcab8d9af2ed62e9768feaeec33a
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-02-07 09:40:38 -0300

    Sending page directly and max values in pagination dto

--- src/querying/dto/pagination.dto.ts
+++ src/querying/dto/pagination.dto.ts
@@ -1,12 +1,18 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, Max } from 'class-validator';
 import { IsCardinal } from 'common/decorators/validators/is-cardinal.decorator';
+import {
+  MAX_PAGE_NUMBER,
+  MAX_PAGE_SIZE,
+} from 'querying/util/querying.constants';
 
 export class PaginationDto {
   @IsOptional()
+  @Max(MAX_PAGE_SIZE)
   @IsCardinal()
   readonly limit?: number;
 
   @IsOptional()
+  @Max(MAX_PAGE_NUMBER)
   @IsCardinal()
-  readonly offset?: number;
+  readonly page?: number = 1;
 }
--- src/querying/util/querying.constants.ts
+++ src/querying/util/querying.constants.ts
@@ -1,3 +1,6 @@
+export const MAX_PAGE_SIZE = 100;
+export const MAX_PAGE_NUMBER = 25;
+
 export const DefaultPageSize = {
   USER: 10,
   ORDER: 5,
