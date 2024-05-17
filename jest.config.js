141646aaaa469cf6b0d9fa0d3490c0e5b068d9c0
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-05-17 12:23:35 -0300

    Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
---
+++ test/jest-e2e.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+const modulePath = '<rootDir>/../src';
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  modulePaths: [modulePath],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: modulePath,
+  }),
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
