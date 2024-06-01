894097b608090d06d9d60c77394db5e3e357f76f
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-06-01 11:39:07 -0300

    Creating payment model

---
+++ prisma/migrations/20240601143636_create_payment/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "orderId" INTEGER NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payment_orderId_key" ON "Payment"("orderId");
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './domain/users/users.module';
 import { CommonModule } from './common/common.module';
 import { OrdersModule } from './domain/orders/orders.module';
+import { PaymentsModule } from './domain/payments/payments.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, CommonModule, OrdersModule],
+  imports: [DatabaseModule, UsersModule, CommonModule, OrdersModule, PaymentsModule],
 })
 export class AppModule {}
