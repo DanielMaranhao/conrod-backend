307d34a97f95dee177a6f5cda94f99b210649905
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-06-01 16:03:46 -0300

    Implementing product logic

--- src/domain/products/dto/create-product.dto.ts
+++ src/domain/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { ArrayNotEmpty, IsCurrency, IsOptional, Length } from 'class-validator';
+import { IsEntity } from 'common/decorators/is-entity.decorator';
+import { IdDto } from 'common/dto/id.dto';
+
+export class CreateProductDto {
+  @Length(2, 50)
+  readonly name: string;
+
+  @IsOptional()
+  @Length(1, 500)
+  readonly description?: string;
+
+  @IsCurrency()
+  readonly price: number;
+
+  @ArrayNotEmpty()
+  @IsEntity()
+  readonly categories: IdDto[];
+}
--- src/domain/products/products.service.ts
+++ src/domain/products/products.service.ts
@@ -1,27 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { PaginationDto } from 'common/dto/pagination.dto';
+import { PrismaService } from 'nestjs-prisma';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { categories } = createProductDto;
+
+    return this.prisma.product.create({
+      data: {
+        ...createProductDto,
+        categories: { connect: categories },
+      },
+    });
   }
 
   findAll(paginationDto: PaginationDto) {
-    return `This action returns all products`;
+    return this.prisma.product.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.prisma.product.findUniqueOrThrow({
+      where: { id },
+      include: {
+        categories: true,
+      },
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    const { categories } = updateProductDto;
+
+    return this.prisma.product.update({
+      where: { id },
+      data: {
+        ...updateProductDto,
+        categories: { set: categories },
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    return this.prisma.product.delete({ where: { id } });
   }
 }
