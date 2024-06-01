43ba24f91840b762854f26473c77e17b23f83e5f
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-05-31 22:31:13 -0300

    Creating user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
 })
 export class AppModule {}
