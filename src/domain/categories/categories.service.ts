e6023b345120ee62725a22716dbe2ec7ccac6f81
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-06-01 14:11:57 -0300

    Implementing category logic

--- src/domain/categories/categories.service.ts
+++ src/domain/categories/categories.service.ts
@@ -1,27 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PaginationDto } from 'common/dto/pagination.dto';
+import { PrismaService } from 'nestjs-prisma';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    return this.prisma.category.create({ data: createCategoryDto });
   }
 
   findAll(paginationDto: PaginationDto) {
-    return `This action returns all categories`;
+    return this.prisma.category.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.prisma.category.findUniqueOrThrow({
+      where: { id },
+      include: {
+        products: true,
+      },
+    });
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.findOne(id);
+    if (category.products.length) {
+      throw new ConflictException('Category has related products');
+    }
+    return this.prisma.category.delete({ where: { id } });
   }
 }
