18ad0b19709587a78f369badd2036911a1ddb4ea
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-06-01 09:03:17 -0300

    Implementing user logic

--- src/domain/users/users.service.ts
+++ src/domain/users/users.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PaginationDto } from 'common/dto/pagination.dto';
+import { PrismaService } from 'nestjs-prisma';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({ data: createUserDto });
   }
 
   findAll(paginationDto: PaginationDto) {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUniqueOrThrow({ where: { id } });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.prisma.user.update({ where: { id }, data: updateUserDto });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prisma.user.delete({ where: { id } });
   }
 }
