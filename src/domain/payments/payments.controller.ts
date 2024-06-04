19c2cb9c411a7bf4d373488d6859f390c0837abc
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-06-04 13:43:44 -0300

    Implementing payment logic

--- src/domain/payments/payments.controller.ts
+++ src/domain/payments/payments.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { IdDto } from 'common/dto/id.dto';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post(':id')
+  payOrder(@Param() { id }: IdDto) {
+    return this.paymentsService.payOrder(id);
+  }
 }
--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { OrderStatus } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async payOrder(id: number) {
+    const { payment } = await this.prisma.order.findUniqueOrThrow({
+      where: { id },
+      include: { payment: true },
+    });
+    if (payment) {
+      throw new ConflictException('Order already paid');
+    }
+
+    return this.prisma.order.update({
+      where: { id },
+      data: {
+        status: OrderStatus.AWAITING_SHIPMENT,
+        payment: { create: {} },
+      },
+    });
+  }
+}
