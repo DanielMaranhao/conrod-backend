88c025d501d03c7b86d206f8ee63dd3e699244a0
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-12-19 17:26:46 -0300

    Use as const and satisfies in ValidationPipe options

--- src/common/util/common.constants.ts
+++ src/common/util/common.constants.ts
@@ -1,10 +1,10 @@
 import { ValidationPipeOptions } from '@nestjs/common';
 
-export const VALIDATION_PIPE_OPTIONS: ValidationPipeOptions = {
+export const VALIDATION_PIPE_OPTIONS = {
   whitelist: true,
   forbidNonWhitelisted: true,
   transform: true,
   transformOptions: {
     enableImplicitConversion: true,
   },
-};
+} as const satisfies ValidationPipeOptions;
