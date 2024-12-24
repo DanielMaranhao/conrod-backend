5149bf0041c79df6626260233f37dd574d58ad7a
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-12-24 08:39:25 -0300

    Refactor: relocate MAX_INT_32 validator to @Cardinal() decorator

--- src/common/decorators/validators/is-cardinal.decorator.ts
+++ src/common/decorators/validators/is-cardinal.decorator.ts
@@ -1,8 +1,13 @@
 import { applyDecorators } from '@nestjs/common';
-import { IsInt, IsPositive, ValidationOptions } from 'class-validator';
+import { IsInt, IsPositive, Max, ValidationOptions } from 'class-validator';
+import { MAX_INT_32 } from 'common/util/common.constants';
 
-/** Checks if the value is a positive integer greater than zero. */
+/** Checks if the value is a positive integer greater than zero, lower than the max value for INT_32. */
 export const IsCardinal = (
   validationOptions?: ValidationOptions,
 ): PropertyDecorator =>
-  applyDecorators(IsInt(validationOptions), IsPositive(validationOptions));
+  applyDecorators(
+    IsInt(validationOptions),
+    IsPositive(validationOptions),
+    Max(MAX_INT_32, validationOptions),
+  );
--- src/common/dto/id.dto.ts
+++ src/common/dto/id.dto.ts
@@ -1,9 +1,6 @@
-import { Max } from 'class-validator';
 import { IsCardinal } from 'common/decorators/validators/is-cardinal.decorator';
-import { MAX_INT_32 } from 'common/util/common.constants';
 
 export class IdDto {
-  @Max(MAX_INT_32)
   @IsCardinal()
   readonly id: number;
 }
