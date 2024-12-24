2875b656a892e6f937dee72729c63db302968ac3
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-12-24 07:38:46 -0300

    Fix: add IsDefined() to @IsEntity()

--- src/common/decorators/validators/is-entity.decorator.ts
+++ src/common/decorators/validators/is-entity.decorator.ts
@@ -1,11 +1,12 @@
 import { applyDecorators } from '@nestjs/common';
 import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { IsDefined, ValidateNested } from 'class-validator';
 import { IdDto } from 'common/dto/id.dto';
 
 /** Checks if the value is an object with only a serial id. */
 export const IsEntity = (): PropertyDecorator =>
   applyDecorators(
+    IsDefined(),
     ValidateNested(),
     Type(() => IdDto),
   );
