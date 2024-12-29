50eb3cb3f4bd2c754d9795ad80870feadd8e7268
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2024-12-29 20:58:30 -0300

    Enforce that sort array should contain product fields

--- src/domain/products/dto/querying/sort-products.dto.ts
+++ src/domain/products/dto/querying/sort-products.dto.ts
@@ -1,7 +1,8 @@
 import { IsIn, IsOptional } from 'class-validator';
+import { Product } from 'products/entities/product.entity';
 import { OrderDto } from 'querying/dto/order.dto';
 
-const Sort = ['name', 'price'] as const;
+const Sort = ['name', 'price'] as const satisfies (keyof Product)[];
 type Sort = (typeof Sort)[number];
 
 export class SortProductsDto extends OrderDto {
