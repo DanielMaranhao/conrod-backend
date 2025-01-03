8b1dc7419fcd12d8e7b3069818b6d506a08e284d
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2025-01-03 14:21:14 -0300

    Use type directly in Sort array

--- src/domain/products/dto/querying/sort-products.dto.ts
+++ src/domain/products/dto/querying/sort-products.dto.ts
@@ -2,7 +2,7 @@ import { IsIn, IsOptional } from 'class-validator';
 import { Product } from 'products/entities/product.entity';
 import { OrderDto } from 'querying/dto/order.dto';
 
-const Sort = ['name', 'price'] as const satisfies (keyof Product)[];
+const Sort: (keyof Product)[] = ['name', 'price'] as const;
 type Sort = (typeof Sort)[number];
 
 export class SortProductsDto extends OrderDto {
