aaab19654af608d2d0314deda0977a5bef0aa525
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2025-01-05 15:27:00 -0300

    Relocate max postgres int validation to IdDto

--- src/common/decorators/validators/is-cardinal.decorator.ts
+++ src/common/decorators/validators/is-cardinal.decorator.ts
@@ -1,13 +1,8 @@
 import { applyDecorators } from '@nestjs/common';
-import { IsInt, IsPositive, Max, ValidationOptions } from 'class-validator';
-import { MAX_INT_32 } from 'common/util/common.constants';
+import { IsInt, IsPositive, ValidationOptions } from 'class-validator';
 
-/** Checks if the value is a positive integer greater than zero, lower than the max value for INT_32. */
+/** Checks if the value is a positive integer greater than zero. */
 export const IsCardinal = (
   validationOptions?: ValidationOptions,
 ): PropertyDecorator =>
-  applyDecorators(
-    IsInt(validationOptions),
-    IsPositive(validationOptions),
-    Max(MAX_INT_32, validationOptions),
-  );
+  applyDecorators(IsInt(validationOptions), IsPositive(validationOptions));
