0ec535b77adbf78fd43ad90f8e9bc765ae0f35b9
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2025-01-10 06:59:39 -0300

    Use the type for the Sort array as satisfies again

--- src/domain/products/dto/querying/sort-products.dto.ts
+++ src/domain/products/dto/querying/sort-products.dto.ts
@@ -2,7 +2,7 @@ import { IsIn, IsOptional } from 'class-validator';
 import { Product } from 'products/entities/product.entity';
 import { OrderDto } from 'querying/dto/order.dto';
 
-const Sort: (keyof Product)[] = ['name', 'price'] as const;
+const Sort = ['name', 'price'] as const satisfies (keyof Product)[];
 type Sort = (typeof Sort)[number];
 
 export class SortProductsDto extends OrderDto {
