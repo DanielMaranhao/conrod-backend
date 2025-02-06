7649efb7c55c58b63ca86a775b1f22414ab370b6
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2025-02-06 06:23:34 -0300

    Format

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { CronModule } from './cron/cron.module';
 import { DatabaseModule } from './database/database.module';
+import { DocsModule } from './docs/docs.module';
 import { CategoriesModule } from './domain/categories/categories.module';
 import { OrdersModule } from './domain/orders/orders.module';
 import { PaymentsModule } from './domain/payments/payments.module';
@@ -9,12 +11,10 @@ import { ProductsModule } from './domain/products/products.module';
 import { UsersModule } from './domain/users/users.module';
 import { EnvModule } from './env/env.module';
 import { FilesModule } from './files/files.module';
-import { QueryingModule } from './querying/querying.module';
-import { DocsModule } from './docs/docs.module';
-import { StaticModule } from './static/static.module';
-import { MailModule } from './mail/mail.module';
 import { HealthModule } from './health/health.module';
-import { CronModule } from './cron/cron.module';
+import { MailModule } from './mail/mail.module';
+import { QueryingModule } from './querying/querying.module';
+import { StaticModule } from './static/static.module';
 
 @Module({
   imports: [
@@ -24,16 +24,16 @@ import { CronModule } from './cron/cron.module';
     AuthModule,
     FilesModule,
     QueryingModule,
-    UsersModule,
-    OrdersModule,
-    PaymentsModule,
-    CategoriesModule,
-    ProductsModule,
     DocsModule,
     StaticModule,
     MailModule,
     HealthModule,
     CronModule,
+    UsersModule,
+    OrdersModule,
+    PaymentsModule,
+    CategoriesModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}
